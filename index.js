01d8c4bba875eb918f3b0364cb895d94a83cacfd
Kyle Burchesky <kyle.burchesky@gmail.com>
2019-08-31 13:52:43 -0400

    Modify classifier to detect rock paper scissors lizard spock dog bomb bront spider

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const classifier = knnClassifier.create();
 const webcamElement = document.getElementById('webcam');
 let net;
+
 
 async function app() {
   console.log('Loading mobilenet..');
@@ -8,22 +10,48 @@ async function app() {
   net = await mobilenet.load();
   console.log('Sucessfully loaded model');
 
-  // Make a prediction through the model on our image.
   await setupWebcam();
+
+  // Reads an image from the webcam and associates it with a specific class
+  // index.
+  const addExample = classId => {
+    // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+    // to the KNN classifier.
+    const activation = net.infer(webcamElement, 'conv_preds');
+
+    // Pass the intermediate activation to the classifier.
+    classifier.addExample(activation, classId);
+  };
+
+  // When clicking a button, add an example for that class.
+  document.getElementById('class-rock').addEventListener('click', () => addExample(0));
+  document.getElementById('class-paper').addEventListener('click', () => addExample(1));
+  document.getElementById('class-scissors').addEventListener('click', () => addExample(2));
+  document.getElementById('class-lizard').addEventListener('click', () => addExample(3));
+  document.getElementById('class-spock').addEventListener('click', () => addExample(4));
+  document.getElementById('class-dog').addEventListener('click', () => addExample(5));
+  document.getElementById('class-bomb').addEventListener('click', () => addExample(6));
+  document.getElementById('class-bronto').addEventListener('click', () => addExample(7));
+  document.getElementById('class-spider').addEventListener('click', () => addExample(8));
+
   while (true) {
-    const result = await net.classify(webcamElement);
+    if (classifier.getNumClasses() > 0) {
+      // Get the activation from mobilenet from the webcam.
+      const activation = net.infer(webcamElement, 'conv_preds');
+      // Get the most likely class and confidences from the classifier module.
+      const result = await classifier.predictClass(activation);
 
-    document.getElementById('console').innerText = `
-      prediction: ${result[0].className}\n
-      probability: ${result[0].probability}
-    `;
+      const classes = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock', 'Dog', 'Bomb', 'Bronto', 'Spider'];
+      document.getElementById('console').innerText = `
+        prediction: ${classes[result.classIndex]}\n
+        probability: ${result.confidences[result.classIndex]}
+      `;
+    }
 
-    // Give some breathing room by waiting for the next animation frame to
-    // fire.
     await tf.nextFrame();
   }
-
 }
+
 async function setupWebcam() {
   return new Promise((resolve, reject) => {
     const navigatorAny = navigator;
